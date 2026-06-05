a71db5db23f2c316b8bc5a30177efdad9ba23afc
Your Name <edkwak@ucsc.edu>
2026-06-04 23:54:06 -0700

    increased friction

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -6,7 +6,7 @@ class Platformer extends Phaser.Scene {
     init() {
         // variables and settings
         this.ACCELERATION = 400;
-        this.DRAG = 500;    // DRAG < ACCELERATION = icy slide
+        this.DRAG = 1000;    // DRAG < ACCELERATION = icy slide
         this.physics.world.gravity.y = 1500;
         this.JUMP_VELOCITY = -600;
         this.PARTICLE_VELOCITY = 50;
